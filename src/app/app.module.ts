6b5c09bb5fe0a451679572dd37be391631b9c27e
johnquintero <johnjairoqp@gmail.com>
2020-08-20 10:56:53 -0500

    Menu and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
